55ba919dd3bf37c8573b7ec938b97f9f257be396
David Buxton <david@gasmark6.com>
2022-08-09 15:17:05 +0100

    Use 1 handler for all 3 routes

--- app-engine-login-required/main.py
+++ app-engine-login-required/main.py
@@ -10,23 +10,11 @@ app = flask.Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
 
-def request_headers():
-    return dict(flask.request.headers)
-
-
 @app.route("/admin")
-def admin():
-    return {'headers': request_headers()}
-
-
 @app.route("/required")
-def required():
-    return {'headers': request_headers()}
-
-
 @app.route("/")
 def public():
-    return {'headers': request_headers()}
+    return {'headers': dict(flask.request.headers)}
 
 
 app = wrap_wsgi_app(app)
