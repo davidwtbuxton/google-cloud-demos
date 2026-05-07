3f55c325fa0926f2dc6d0b1729ca9fbb5382ca4f
David Buxton <david@gasmark6.com>
2026-05-07 16:52:38 +0100

    Updates for generated signed storage URLs
    Fixed the demo to work on the Python 3.14 runtime, and removed the
    extra steps of creating a specific Credential class. I never knew the
    Python google-cloud-storage package lets you pass in the service
    account and access token, way simpler.

--- signing-storage-urls/main.py
+++ signing-storage-urls/main.py
@@ -1,32 +1,13 @@
 import datetime
+import json
 import os
 
-import flask
 import google.auth
-import google.auth.compute_engine
 import google.auth.transport.requests
-import google.oauth2.service_account
 from google.cloud import storage
 
 
-app = flask.Flask(__name__)
-app.config['JSON_PRETTYPRINT_REGULAR'] = True
 EXPIRATION = datetime.timedelta(seconds=60)
-
-
-def new_creds():
-    """Create credentials that can sign blobs for a service account.
-
-    You must have enabled the iamcredentials.googleapis.com API, and granted
-    the default service account the token creator role for itself.
-    """
-    # Assumes we are running on App Engine.
-    request = google.auth.transport.requests.Request()
-    creds = google.auth.compute_engine.IDTokenCredentials(request, None)
-    creds = google.oauth2.service_account.Credentials(
-        creds.signer, creds.service_account_email, creds._token_uri)
-
-    return creds
 
 
 def get_bucket_name():
@@ -36,19 +17,29 @@ def get_bucket_name():
     return f'{project}.appspot.com'
 
 
-@app.route('/')
 def list_objects():
     """Show a list of objects in the default bucket, with signed URLs.
 
     Results are limited to 20 objects. The URLs are valid for 60 seconds.
     """
-    creds = new_creds()
-    client = storage.Client(credentials=creds)
+    creds, _ = google.auth.default()
+    # Here we fetch the service account email and a short-lived access token
+    # from the metadata service. A more efficient implementation could check
+    # `creds.token_state` to see if a refresh is needed.
+    # https://googleapis.dev/python/google-auth/latest/reference/google.auth.credentials.html#google.auth.credentials.Credentials.token_state
+    creds.refresh(google.auth.transport.requests.Request())
+
+    client = storage.Client()
     bucket = client.bucket(get_bucket_name())
     objects = []
 
     for blob in bucket.list_blobs(max_results=20):
-        signed_url = blob.generate_signed_url(expiration=EXPIRATION, version='v4')
+        signed_url = blob.generate_signed_url(
+            expiration=EXPIRATION,
+            version='v4',
+            service_account_email=creds.service_account_email,
+            access_token=creds.token,
+        )
         objects.append({
             'name': blob.name,
             'signed_url': signed_url,
@@ -56,3 +47,10 @@ def list_objects():
         })
 
     return {'objects': objects}
+
+
+def app(environ, start_response):
+    """The WSGI application entrypoint."""
+    start_response('200 OK', [('content-type', 'application/json')])
+    objects = list_objects()
+    yield json.dumps(objects, indent=2, sort_keys=True).encode('utf-8')
